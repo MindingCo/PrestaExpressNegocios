6fc26e85c96e0d82339c0c029192871ee9525e56
M1ndBlast <gustavopdzz0@gmail.com>
2018-09-26 13:21:49 -0500

    Subida de nuevos archivos

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Presta Express Negocios' });
+});
+router.get('/inicio', function(req, res, next) {
+    res.render('cliente',{
+        title: 'Inicio',
+        prueba: 'Holi'
+    });
 });
 
 module.exports = router;
