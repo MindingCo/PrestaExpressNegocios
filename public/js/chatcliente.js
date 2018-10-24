ff7cf777798d4ead8beef19e4d8047492fe2b29d
M1ndBlast <gustavopdzz0@gmail.com>
2018-10-24 17:55:54 -0500

    funcion para enter de mensajes

--- public/js/chatcliente.js
+++ public/js/chatcliente.js
@@ -2,29 +2,30 @@
 $(document).ready(function()
 {
     var socket = io('http://'+location.hostname+':8081', {forceNew: true})
-
-    $('#enviar').click(function(){
-        alert("Inicio funcion")
+    function mandarMsj()
+    {
         socket.emit('new_message', $('#message-text').val());
-        alert("Llego aca 3")
         $('#message-text').val(null)
         return false
+    }
+    $("#message-text").keypress(function(e)
+    {
+        if(e.which == 13)
+            mandarMsj()
     })
 
+    $('#enviar').click(function(){ mandarMsj() })
+
+
     socket.on('new_user', (newUser) =>{
-      alert(newUser.message)
-      alert("LLego aqui 4")
     })
 
     socket.on('new_message', (mensaje) =>{
       $('#chat').append('<li>' + mensaje + '</li>')
     });
 
-    alert("LLego aqui 5")
     socket.on('user_says', function(userSays){
-      alert("LLego aqui 6")
       $('#chat').append('<li>' + userSays + '</li>')
-      alert("El mensaje es: " + userSays)
     })
 
 })//(document, io, jQuery)
