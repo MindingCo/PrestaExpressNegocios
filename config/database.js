42fe337c8abce6b08831596037205a6a25ab087c
root <root@vps.server.local>
2018-11-22 20:58:24 +0000

    No cambien la contra ya >:V

--- config/database.js
+++ config/database.js
@@ -1,13 +1,9 @@
+
 module.exports = {
     'connection': {
         'host': 'localhost',
-<<<<<<< HEAD
         'user': 'PEN',
         'password': 'PENmysql'
-=======
-        'user': 'root',
-        'password': 'Ilovebr3ad'
->>>>>>> bf1006c494f93d7b7656ce66346bffe24274883d
     },
 	'database': 'pen',
   'users_table':'usuario'
