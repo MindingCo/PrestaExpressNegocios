03a3367b654e7e07bc6547181a5ac81f11f7dcf7
Cracell Craft Hunter <cracellch@gmail.com>
2018-11-27 10:24:44 -0600

    Detalles

--- public/js/canva.js
+++ public/js/canva.js
@@ -1,19 +1,14 @@
+var cant= document.getElementById('valu').innerHTML;
 var ctx = document.getElementById('myChart').getContext('2d');
-          var myChart = new Chart(ctx, {
-            type: 'line',
-            data: {
-              labels: ['L', 'M', 'X', 'J', 'V', 'S', 'D'],
-              datasets: [{
-                label: 'apples',
-                data: [12, 19, 3, 17, 6, 3, 7],
-                backgroundColor: "rgba(26,129,102,0.2)",
-                borderColor: "#3cba9f",
-                fill:false
-              }, {
-                label: 'oranges',
-                data: [2, 29, 5, 5, 2, 3, 10],
-                backgroundColor: "rgba(255,153,0,0.2)",
-                borderColor: "rgba(179,11,198,1)",
-              }]
-            }
-          });
+var myChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: ['Vie.8,Noviembre 2018', 'Sab.9,Noviembre 2018', 'Dom.10,Noviembre 2018', 'Lun.11,Noviembre 2018', 'Mar.12,Noviembre 2018', 'Mie.13,Noviembre 2018', 'Jue.14,Noviembre 2018'],
+    datasets: [{
+      label: 'Pagos Clientes',
+      data: [cant,150,250,180,100,50,190],
+      backgroundColor: "rgba(255,153,0,0.2)",
+      borderColor: "rgba(179,11,198,1)",
+    }]
+  }
+});
